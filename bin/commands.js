846f3a59a4bd3927f21639f0f7f6062ec6aa617e
iClusterDev <cluster.coredev@gmail.com>
2020-11-19 18:15:48 +0000

    core functions added

--- bin/commands.js
+++ bin/commands.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
 
+const program = require('commander');
+const create = require('../lib/index');
+
+program.version('1.0.0').description('DevKit CLI');
+
+program
+  .command('create <projectName>')
+  .description('create project')
+  .action(async (projectName) => {
+    await create(projectName);
+  });
+
+program.parse(process.argv);
